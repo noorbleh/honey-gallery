a5eedf87f87647c9a8460d7acf06fc5b6280a8e7
noorbleh <arshnoor1740@gmail.com>
2025-12-03 20:44:35 +0530

    Updated Cloudinary photo links and added ambient audio to public folder

--- frontend/app/gallery/cg/page.tsx
+++ frontend/app/gallery/cg/page.tsx
@@ -75,7 +75,7 @@ export default function CgCollection() {
 
       if (!audio) {
         audio = document.createElement("audio");
-        audio.src = "https://res.cloudinary.com/doqshkcln/raw/upload/v1764772522/ambient_obqtzy.mp3"
+        audio.src = "/ambient.mp3";
         audio.id = "ambient-audio";
         audio.loop = true;
         audio.volume = 0.17;
